b45bdaf301cba904f1aa532af3fc7f5a861fc58e
Jordan <jordanroberts333@icloud.com>
2023-02-13 09:47:58 +0000

    css scroll-snapping

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="h-full overflow-y-scroll">
-      okay
+    <div class="snap-y snap-mandatory h-full overflow-y-scroll">
+      <section class="snap-start h-[100vh]">hello</section>
+      <section class="snap-start h-[100vh]">hrrr</section>
+      <section class="snap-start h-[100vh]">heksssllo</section>
     </div>
   )
 }
