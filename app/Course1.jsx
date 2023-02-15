3f1a7b87a3ea9dfce1c430cedb46a62e9bb17f15
Jordan <jordanroberts333@icloud.com>
2023-02-15 22:43:05 +0000

    testing styles on mobile

--- app/Course1.jsx
+++ app/Course1.jsx
@@ -3,9 +3,10 @@ import OnloadImage from './OnloadImage'
 
 export default function Course1(){
     return (
-      <section className="snap-start h-[100%]">
-        <div className='relative w-full h-[100%] bg-black'>
+      <section className="snap-start h-[100lvh]">
+        <div className='relative w-full h-[100%] bg-green-500 overflow-y-auto'>
             <OnloadImage src={mathImage} alt={'not math'} priority={true}/>
+            <div className='h-[400vh]'>hi</div>
         </div>
     </section>
     )
--- app/Overview.jsx
+++ app/Overview.jsx
@@ -8,13 +8,11 @@ export default function Overview(){
   const [maskOn, setMaskOn] = useState(false)
     // useStore.setState({price : 2})
   return (
-    <section className="snap-start h-[100%] bg-black" onClick={() => setMaskOn((prev) => !prev)}>
+    <section className="snap-start h-[100svh] bg-black" onClick={() => setMaskOn((prev) => !prev)}>
         {/* {useStore.getState().price} */}
         <div className='relative w-full h-[100%]'>
             <OnloadImage src={mathImage} alt={'not math'} priority={true} mask={maskOn}/>
-            <div className='fixed left-0 h-full w-[40vw] goon shadow-md z-50'>
-
-            </div>
+            
         </div>
     </section>
   )
