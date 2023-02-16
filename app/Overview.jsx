3d002ddf9f760f22965194473231c5994ba73076
Jordan <jordanroberts333@icloud.com>
2023-02-16 10:23:58 +0000

    scroll after transition

--- app/Overview.jsx
+++ app/Overview.jsx
@@ -13,18 +13,19 @@ export default function Overview(){
   let image2 = useRef(null)
   let wrapper = useRef(null)
   let toggle = useRef(true)
+  let course = useRef(null)
   const rumble = () => {
     if(toggle.current){
       toggle.current = !toggle.current
       image2.current.style.width = '0%'
       image2.current.style.scale = 1.1
-      image1.current.style.width = '100%'
+      course.current.style.width = '100%'
       image1.current.style.scale = 1
     }else{
       toggle.current = !toggle.current
       image2.current.style.width = '100%'
       image2.current.style.scale = 1
-      image1.current.style.width = '0%'
+      course.current.style.width = '0%'
       image1.current.style.scale = 1.1
     }
   }
@@ -32,7 +33,10 @@ export default function Overview(){
     <section className="snap-start h-[100svh] bg-black" onClick={() => rumble()}>
         {/* {useStore.getState().price} */}
         <div ref={wrapper} className='relative w-full h-[100%] duration-1000'>
-          <Image ref={image1} src={codingImage} alt={'not math'} className='w-[0%] h-full duration-[1s] right-0 absolute object-cover scale-110 border-l-2 border-white'/>
+          <div ref={course} className='w-[0%] h-full duration-[1s] right-0 absolute border-l-2 border-white overflow-y-auto'>
+            <Image ref={image1} src={codingImage} alt={'not math'} fill className='object-cover scale-110 duration-1000'/>
+            <div className='top-0 w-full bg-purple-300 h-[400vh]'></div>
+          </div>
           <Image ref={image2} src={mathImage} alt={'not math'} fill className='w-[100%] duration-[1s] object-cover border-r-2 border-white'/>
             {/* <OnloadImage src={mathImage} alt={'not math'} priority={true} mask={maskOn}/> */}
         </div>
